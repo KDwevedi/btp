9d4c98e0706b43eb31a485957f2909c9f2da4876
KDwevedi <kanav11dwevedi@gmail.com>
2023-05-15 23:00:25 +0530

    Feat: styling

--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,6 @@
 
 
+
 export default function Signin () {
     function makeid(length) {
         let result = '';
@@ -40,24 +41,24 @@ export default function Signin () {
     }
     return (
 
-<section class="text-gray-600 body-font">
-    
+      <section class="text-gray-600 body-font">
   <div id="api" class="container px-5 py-24 mx-auto">
-    <h1 class="text-3xl font-medium title-font text-gray-900 mb-12 text-center">API</h1>
-    <div >
-        Sign-in to get your API key
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">API Portal</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Sign-in to get your API key</p>
     </div>
-    <form class="signin">
-      <div class="form-group">
-      <div id="showapi"></div>
-        <input type="email" class="form-control" id="inptemail" aria-describedby="emailHelp" placeholder="Enter email"></input>
-
-<input type="password" class="form-control" id="inptpassword" aria-describedby="emailHelp" placeholder="Enter Password" ></input>
-
+    <div id="showapi"></div>
+    <div class="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+      <div class="relative flex-grow w-full">
+        <label for="full-name" class="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="inptemail" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <button class="btn btn-primary signinbtn" onClick={signinbtn}>Continue</button>
-    </form>
-    
+      <div class="relative flex-grow w-full">
+        <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
+        <input type="password" id="inptpassword" name="password" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button class="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg" onClick={signinbtn}>Continue</button>
+    </div>
   </div>
 </section>
 
